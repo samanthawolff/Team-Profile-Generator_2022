ed93834cbb80001f9e86602d43e5835842bcbdca
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-13 17:17:28 -0500

    passing test for employee id

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Samantha', '20', 'samanthawolff07@gmail.com');
+    const employee = new Employee('Samantha', 20, 'samanthawolff07@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
@@ -9,7 +9,13 @@ test('creates an employee object', () => {
 });
 
 test('get employee name', () => {
-    const employee = new Employee('Samantha', '20', 'samanthawolff07@gmail.com');
+    const employee = new Employee('Samantha', 20, 'samanthawolff07@gmail.com');
 
     expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('get employee id', () => {
+    const employee = new Employee('Samantha', 20, 'samanthawolff07@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
 });
