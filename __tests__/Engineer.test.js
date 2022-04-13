434bee1502188ce6a994fca3e51e249931a77d52
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-13 18:14:06 -0500

    passing tests for engineer getGithub and getRole

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,3 +5,15 @@ test('create an engineer object', () => {
 
     expect(engineer.github).toEqual(expect.any(String));
 });
+
+test('get engineer github', () => {
+    const engineer = new Engineer('Samantha', 20, 'samanthawolff07@gmail.com', 'samanthawolff');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('get role of employee', () => {
+    const engineer = new Engineer('Samantha', 20, 'samanthawolff07@gmail.com', 'samanthawolff');
+
+    expect(engineer.getRole()).toEqual("Engineer");
+});
