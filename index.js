02c453f0eed715d8fc977dcf628ce72853b7b394
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-13 19:30:56 -0500

    add manager name and id questions

--- index.js
+++ index.js
@@ -13,7 +13,38 @@ const Engineer = require('./lib/Engineer');
 // Empty team array
 const teamMembers = []
 
+const teamManager = () => {
+    return inquirer.prompt([
 
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is the team managers name?',
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the managers name')
+                    return false;
+                };
+            },
+        },
 
+        {
+            type: 'input',
+            name: 'ID',
+            message: "What is the managers ID?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the managers ID')
+                    return false;
+                };
+            },
+        },
+    ])
+}
 
+teamManager();
 
